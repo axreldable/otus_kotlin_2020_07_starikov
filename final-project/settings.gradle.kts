03f971be8cfccf0517c449c4a54390aebb4d8cc8
axreldable <starikovaleksei1992@mail.ru>
2020-08-22 13:33:38 +0300

    hw_2: rm pluginManagement cause it doesn't work for some reason

--- final-project/settings.gradle.kts
+++ final-project/settings.gradle.kts
@@ -1,10 +1,2 @@
 rootProject.name = "final-project"
 include("comment-common")
-
-//pluginManagement {
-//    plugins {
-//        val kotlinVersion: String by settings
-//
-//        kotlin("jvm") version kotlinVersion apply false
-//    }
-//}
